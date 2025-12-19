122ed71ac7514e21e40e1e533dc8e35ad483d298
ivis-kuroda <tomohiro.kuroda@ivis.co.jp>
2025-12-19 04:16:05 +0000

    test: setup pytest and add tests for MapUser schema

---
+++ tests/helpers.py
@@ -0,0 +1,8 @@
+import json
+from pathlib import Path
+from typing import Any
+
+
+def load_json_data(file_path: str) -> dict[str, Any]:
+    with (Path(__file__).parent / file_path).open() as file:
+        return json.load(file)
